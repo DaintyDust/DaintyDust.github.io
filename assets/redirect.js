820f2373d43c323ee09158003382cdfe894a2867
DaintyDust <144209886+DaintyDust@users.noreply.github.com>
2024-06-14 19:12:18 +0200

    Update redirect.js

--- assets/redirect.js
+++ assets/redirect.js
@@ -9,39 +9,9 @@ const redirects = {
     "/group": "https://www.roblox.com/groups/8193767/Nick-Studios#!/about",
 };
 
-function handleRedirect() {
-    const path = window.location.pathname;
-    console.log(path);
-    console.log(redirects[path]);
-    if (redirects[path]) {
-        window.location.href = redirects[path];
-    }
+const path = window.location.pathname;
+console.log(path)
+console.log(redirects[path])
+if (redirects[path]) {
+    window.location.href = redirects[path];
 }
-
-// Listen for URL changes
-window.addEventListener('popstate', handleRedirect);
-
-// Override pushState and replaceState to detect URL changes
-(function(history) {
-    const pushState = history.pushState;
-    const replaceState = history.replaceState;
-
-    history.pushState = function(state, title, url) {
-        if (typeof history.onpushstate == "function") {
-            history.onpushstate({state: state});
-        }
-        handleRedirect();
-        return pushState.apply(history, arguments);
-    };
-
-    history.replaceState = function(state, title, url) {
-        if (typeof history.onreplacestate == "function") {
-            history.onreplacestate({state: state});
-        }
-        handleRedirect();
-        return replaceState.apply(history, arguments);
-    };
-})(window.history);
-
-// Initial redirect check
-handleRedirect();
